0282789d24f536929ac9046167e2d4eafbc3a1b4
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-04 00:00:40 +0800

    user input

--- pet class/test_pet.py
+++ pet class/test_pet.py
@@ -2,6 +2,14 @@
 # BSCPE 1-5
 
 # import Pet class
+from pet_class import Pet
+
 # promt user for name, type, and age of their pet
+name = input("Enter the name of your pet: ")
+type = input("Enter what type of animal is your pet: ")
+age = input("Enter the age of your pet: ")
+
 # initialize parameter
+
+
 # create object
