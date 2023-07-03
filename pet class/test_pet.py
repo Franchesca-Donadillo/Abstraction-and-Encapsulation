67731c777fdd60faefb9bec46ae3bcfff6f3b079
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-04 00:55:15 +0800

    adjustments

--- pet class/test_pet.py
+++ pet class/test_pet.py
@@ -5,9 +5,10 @@
 from pet_class import Pet
 
 # promt user for name, type, and age of their pet
-name = input("Enter the name of your pet: ")
-type = input("Enter what type of animal is your pet: ")
-age = input("Enter the age of your pet: ")
+print("\n" + "INPUT".center(30))
+name = input("Enter the name of your pet: ").upper()
+type = input("Enter what type of animal is your pet: ").upper()
+age = input("Enter the age of your pet: ").upper()
 
 # initialize parameter
 pet = Pet(name = str(), animal_type = str(), age = int())
@@ -16,7 +17,9 @@ pet = Pet(name = str(), animal_type = str(), age = int())
 your_pet = Pet(name, type, age)
 
 # display
+print("\n" + "="*25 + "\n" +"PET INFORMATION".center(30))
 your_pet.set_name()
 your_pet.set_animal_type()
 your_pet.set_age()
+print("="*25)
 
