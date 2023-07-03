6b20d2eb6b6a817550052556d60bcd61d657d382
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-03 13:49:41 +0800

    power, radius, color

--- fan class/FanClass.py
+++ fan class/FanClass.py
@@ -8,12 +8,20 @@ class Fan:
     FAST = 3
 
     # initialize 
-    def __init__(self, speed = SLOW, radius = 5, color = "blue", on = False):
+    def __init__(self, on = False, speed = SLOW, radius = 5, color = "blue"):
               
         # private data of speed, power, radius, and color of the fan
         self.__speed = speed
         self.__radius = radius
         self.__color = color
+        self.__on = on
+
+    # get power status
+    def get_power(self):
+        return self.__on
+    
+    # set power status
+    def set_power(self, on):
         self.__on = on
 
     # get for speed
@@ -24,9 +32,18 @@ class Fan:
     def set_speed(self, speed):
         self.__speed = speed
                     
-    # get power status
-    # set power status
     # get radius
+    def get_radius(self):
+        return self.__radius
+    
     # set radius
+    def set_radius(self, radius):
+        self.__radius = radius
+
     # get color
+    def get_color(self):
+        return self.__color
+    
     # set color
+    def set_color(self, color):
+        self.__color = color
