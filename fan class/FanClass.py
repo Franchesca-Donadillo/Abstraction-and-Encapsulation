1fcb319dc9912c44cb99953a39615e9f5c987cb7
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-03 13:11:20 +0800

    initialize

--- fan class/FanClass.py
+++ fan class/FanClass.py
@@ -2,13 +2,25 @@
 # BSCPE 1-5
 
 # Create class
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
     # initialize 
-    # private data of speed, power, radius, and color of the fan
-    # get for speed
-    # set speed
-    # get power status
-    # set power status
-    # get radius
-    # set radius
-    # get color
-    # set color
+    def __init__(self, speed = SLOW, radius = 5, color = "blue", on = False):
+              
+        # private data of speed, power, radius, and color of the fan
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
+
+        # get for speed
+        # set speed
+        # get power status
+        # set power status
+        # get radius
+        # set radius
+        # get color
+        # set color
