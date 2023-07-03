6bd60a85e70a5a9f0c0e6e4b6fdea7caf103bbf9
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-03 23:30:40 +0800

    methods

--- pet class/pet_class.py
+++ pet class/pet_class.py
@@ -11,3 +11,20 @@ class Pet:
         self.__age = age
 
     # methods
+    def get_name(self):
+        return self.__name
+
+    def set_name(self):
+        self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def set_animal_type(self):
+        self.__animal_type
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self):
+        self.__age
