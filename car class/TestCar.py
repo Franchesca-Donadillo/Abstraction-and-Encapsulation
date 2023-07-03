38a77e65b4c238da9d56a1bf39c87da20199af8f
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-03 19:57:52 +0800

    parameter

--- car class/TestCar.py
+++ car class/TestCar.py
@@ -2,6 +2,11 @@
 # BSCPE 1-5
 
 # import Car class from CarClass
+from CarClass import Car
+
+# initialize parameter
+car = Car(year_model = int(), make = str(), speed = 0)
+
 # create car object
 # call accelarate 5 times and display current speed
 # call break method 5 times and display current speed
