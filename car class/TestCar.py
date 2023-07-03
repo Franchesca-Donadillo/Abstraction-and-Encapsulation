a66ba5a4bb8417d9d8ed9d67da99d7aee3820733
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-03 19:37:18 +0800

    TestCar pseudocode

---
+++ car class/TestCar.py
@@ -0,0 +1,7 @@
+# Franchesca Marie U. Donadillo
+# BSCPE 1-5
+
+# import Car class from CarClass
+# create car object
+# call accelarate 5 times and display current speed
+# call break method 5 times and display current speed
